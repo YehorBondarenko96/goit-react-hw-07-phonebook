b80805bc4aa977114f4d5552db9fca5771967f80
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-17 14:05:29 +0200

    3.5.1-17.01.24

--- src/redux/opertions.jsx
+++ src/redux/opertions.jsx
@@ -12,7 +12,7 @@ export const fetchContacts = createAsyncThunk('contacts/fetchAll', async(_, thun
     }
 });
 
-export const addContact = createAsyncThunk('/addContact', async(dataForAdd, thunkAPI) => {
+export const addContact = createAsyncThunk('contacts/addContact', async(dataForAdd, thunkAPI) => {
     try{
         const result = await axios.post('/contacts', dataForAdd);
         return result.data;
@@ -21,7 +21,7 @@ export const addContact = createAsyncThunk('/addContact', async(dataForAdd, thun
     }
 });
 
-export const deleteContact = createAsyncThunk('/contacts/deleteContact', async(contactId, thunkAPI) => {
+export const deleteContact = createAsyncThunk('contacts/deleteContact', async(contactId, thunkAPI) => {
     try{
         const result = await axios.delete(`/contacts/${contactId}`);
         return result.data;
